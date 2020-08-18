33d1197615c14e455342130b9b612c5f0bd26746
Oscar Oscarson <oscar_oscarson@hotmail.com>
2020-08-18 22:29:58 +0200

    Test data log queue with multiple threads calling.

--- drone/tests/test_data_log_queue.cpp
+++ drone/tests/test_data_log_queue.cpp
@@ -2,8 +2,31 @@
 
 #include <thread>
 #include <cstring>
+#include <task.h>
 #include <wall_time.h>
 #include <data_log_queue.h>
+
+static const double FLOAT_TOL = 1e-4;
+
+DataLogQueue data_log_queue_multi;
+
+class TestTaskOne : public Task
+{
+public:
+    TestTaskOne(uint32_t exec_period_ms, void (*exec_period_exceeded_cb)()) :
+        Task(exec_period_ms, exec_period_exceeded_cb) {}
+protected:
+    void _execute() { data_log_queue_multi.push(1.0, DataLogSignal::ImuAccelerationX); }
+};
+
+class TestTaskTwo : public Task
+{
+public:
+    TestTaskTwo(uint32_t exec_period_ms, void (*exec_period_exceeded_cb)()) :
+        Task(exec_period_ms, exec_period_exceeded_cb) {}
+protected:
+    void _execute() { data_log_queue_multi.push(2.0, DataLogSignal::ImuAccelerationY); }
+};
 
 TEST_CASE("data_log_queue: single thread")
 {
@@ -35,8 +58,47 @@ TEST_CASE("data_log_queue: single thread")
 
 TEST_CASE("data_log_queue: multi thread")
 {
-    // TODO: Launch two tasks and add some data.
-    // TODO: Test what happens if a other type is pushed compared to DataLogSignalInfo.
+    TestTaskOne task_one(10, nullptr); // Runs at 100 Hz
+    TestTaskTwo task_two(20, nullptr); // Runs at 50 Hz
+
+    task_one.launch();
+    task_two.launch();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    task_one.teardown();
+    task_two.teardown();
+
+    REQUIRE(data_log_queue_multi.is_empty() == false);
+
+    uint8_t n_act_one_samples = 0;
+    uint8_t n_act_two_samples = 0;
+
+    double exp_one_sample_data = 1.0;
+    double exp_two_sample_data = 2.0;
+
+    while (!data_log_queue_multi.is_empty())
+    {
+        DataLogSample sample = data_log_queue_multi.pop();
+
+        if ((std::memcmp(&exp_one_sample_data, &sample.data, sizeof(double)) == 0) &&
+             sample.signal == DataLogSignal::ImuAccelerationX)
+        {
+            n_act_one_samples++;
+        }
+        else if ((std::memcmp(&exp_two_sample_data, &sample.data, sizeof(double)) == 0) &&
+                  sample.signal == DataLogSignal::ImuAccelerationY)
+        {
+            n_act_two_samples++;
+        }
+        else
+        {
+            FAIL("Unexpected sample");
+        }
+    }
+
+    REQUIRE(n_act_one_samples >= 10);
+    REQUIRE(n_act_two_samples >= 5);
 }
 
 TEST_CASE("data_log_queue: error handling")
