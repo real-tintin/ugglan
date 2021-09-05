b755108749af16c767817e7718fca505a35af90b
Oscar Goldring <oscar_goldring@outlook.com>
2021-09-05 21:43:40 +0200

    Re-tune pilot ctrl, update doc and tools alongside.

--- tools/state_ctrl/state_space.py
+++ tools/state_ctrl/state_space.py
@@ -15,33 +15,43 @@ class State(Enum):
     PSI = 6  # rotation about z
 
 
-_A = lambda c: np.array([[0, 1, 0],
-                         [0, 0, c],
-                         [0, 0, -1 / TAU]])
+_A = np.array([[0, 1, 0],
+               [0, 0, 1],
+               [0, 0, -1 / TAU_M]])
 
-_B = np.array([[0, 0, 1 / TAU]]).transpose()
+_B = lambda c: np.array([[0, 0, c / TAU_M]]).transpose()
 
-_C = np.array([[1, 0, 0],
-               [0, 1, 0]])
+_C = np.eye(3)
 
 _D = 0
 
-_A_INTG = lambda c: np.array([[0, 1, 0, 0],
-                              [0, 0, 1, 0],
-                              [0, 0, 0, c],
-                              [0, 0, 0, -1 / TAU]])
+_A_INTG = np.array([[0, 1, 0, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1],
+                    [0, 0, 0, -1 / TAU_M]])
 
-_B_INTG = np.array([[0, 0, 0, 1 / TAU]]).transpose()
+_B_INTG = lambda c: np.array([[0, 0, 0, c / TAU_M]]).transpose()
 
-_C_INTG = np.array([[1, 0, 0, 0],
-                    [0, 1, 0, 0],
-                    [0, 0, 1, 0]])
+_C_INTG = np.eye(4)
 
 
-def get_c(state: State) -> float:
+def get_state_space(state: State, add_intg_state: bool = False) -> \
+        (np.array, np.array, np.array, np.array):
     """
-    Returns c (see doc for details) given a StateSpace.
+    Returns a StateSpace (A, B, C, D) of the open-loop
+    drone's dynamics given a State.
+
+    Use add_intg_state to add an integrating state.
     """
+    c = _get_c(state)
+
+    if add_intg_state:
+        return _A_INTG, _B_INTG(c), _C_INTG, _D
+    else:
+        return _A, _B(c), _C, _D
+
+
+def _get_c(state: State) -> float:
     if state == State.X or state == State.Y or state == State.Z:
         c = 1 / MASS
     elif state == State.PHI:
@@ -54,19 +64,3 @@ def get_c(state: State) -> float:
         raise ValueError
 
     return c
-
-
-def get_state_space(state: State, add_intg_state: bool = False) -> \
-        (np.array, np.array, np.array, np.array):
-    """
-    Returns a StateSpace (A, B, C, D) of the open-loop
-    drone's dynamics given a State.
-
-    Use add_intg_state to add an integrating state.
-    """
-    c = get_c(state)
-
-    if add_intg_state:
-        return _A_INTG(c), _B_INTG, _C_INTG, _D
-    else:
-        return _A(c), _B, _C, _D
