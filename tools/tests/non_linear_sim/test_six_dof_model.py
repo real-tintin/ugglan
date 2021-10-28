921048547027319cd6b1e560acc7a789c5836f16
Oscar Goldring <oscar_goldring@outlook.com>
2021-10-28 19:16:15 +0200

    Create and use a unit 6dof model during unit test.

--- tools/tests/non_linear_sim/test_six_dof_model.py
+++ tools/tests/non_linear_sim/test_six_dof_model.py
@@ -14,31 +14,41 @@ STATES_NON_ZERO = States(
 )
 
 
+class UnitSixDofModel(SixDofModel):
+    def __init__(self, states_init: States = STATES_ZERO):
+        super().__init__(mass=1.0, moment_of_inertia=np.eye(3), states_init=states_init)
+
+
 @pytest.fixture
-def model_zero_init():
-    yield SixDofModel()
+def unit_model_zero_init():
+    yield UnitSixDofModel()
+
+
+@pytest.fixture
+def unit_model_non_zero_init():
+    yield UnitSixDofModel()
 
 
 class TestSixDofModel:
 
     def test_init_without_arg(self):
-        model = SixDofModel()
+        model = UnitSixDofModel()
         assert model.get_states() == STATES_ZERO
 
     @pytest.mark.parametrize("states", [STATES_ZERO, STATES_NON_ZERO])
     def test_init_with_arg(self, states):
-        model = SixDofModel(states)
+        model = UnitSixDofModel(states)
         assert model.get_states() == states
 
     def test_step(self):
         # TODO: Test step with various scenarios (zero input, const acc, step acc, back and fourth, ...)
         pass
 
-    def test_reset_without_arg(self, model_zero_init):
-        model_zero_init.reset()
-        assert model_zero_init.get_states() == STATES_ZERO
+    def test_reset_without_arg(self, unit_model_non_zero_init):
+        unit_model_non_zero_init.reset()
+        assert unit_model_non_zero_init.get_states() == STATES_ZERO
 
     @pytest.mark.parametrize("states", [STATES_ZERO, STATES_NON_ZERO])
-    def test_reset_with_arg(self, model_zero_init, states):
-        model_zero_init.reset(states)
-        assert model_zero_init.get_states() == states
+    def test_reset_with_arg(self, unit_model_non_zero_init, states):
+        unit_model_non_zero_init.reset(states)
+        assert unit_model_non_zero_init.get_states() == states
