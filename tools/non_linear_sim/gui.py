eb7648bbbff970c474e730abf86baae41cf8582b
Oscar Goldring <oscar_goldring@outlook.com>
2021-11-11 19:55:02 +0100

    Objectify the gui. But far from done...

--- tools/non_linear_sim/gui.py
+++ tools/non_linear_sim/gui.py
@@ -5,7 +5,7 @@ import numpy as np
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.figure import Figure
 
-FREQ_START = 1
+
 # TODO: Draw first by hand. Refactor in the end!
 
 # TODO: Tab/box for  drone params, env params, att_est tuning, noise (imu + vibrations), pilot_ctrl tuning.
@@ -17,65 +17,70 @@ FREQ_START = 1
 # Use class design pattern e.g., https://www.pythontutorial.net/tkinter/tkinter-after/: Create an app to call.
 # Make use of menu?
 
-root = tk.Tk()
-root.title("Non-linear 6dof simulator")
-tabControl = ttk.Notebook(root)
+class Gui(tk.Tk):
+    PLOT_RATE_MS = 10
 
-tab1 = ttk.Frame(tabControl)
-tab2 = ttk.Frame(tabControl)
+    def __init__(self):
+        super().__init__()
 
-tabControl.add(tab1, text='Tab 1')
-tabControl.add(tab2, text='Tab 2')
-tabControl.pack(expand=1, fill="both")
+        self.title("Non-linear 6dof simulator")
 
-ttk.Label(tab1,
-          text="Welcome to \
-          GeeksForGeeks").grid(column=0,
-                               row=0,
-                               padx=30,
-                               pady=30)
+        self._setup_tabs()
+        self._setup_plot()
 
-fig = Figure(figsize=(5, 4), dpi=100)
-t = np.arange(0, 3, .01)
-sine, = fig.add_subplot(111).plot(t, np.sin(FREQ_START * np.pi * t))
+    def _setup_tabs(self):
+        self._tab_ctrl = ttk.Notebook(self)
 
+        self._tab_0 = ttk.Frame(self._tab_ctrl)
+        self._tab_1 = ttk.Frame(self._tab_ctrl)
 
+        self._tab_ctrl.add(self._tab_0, text='Tab 1')
+        self._tab_ctrl.add(self._tab_1, text='Tab 2')
 
+        self._tab_ctrl.pack(expand=1, fill="both")
 
-def _update_plot():
-    _update_plot.freq = np.mod(_update_plot.freq + 0.01, 10)
-    sine.set_ydata(np.sin(_update_plot.freq * np.pi * t))
-    canvas.draw()
-    canvas.get_tk_widget().after(10, _update_plot)
+        ttk.Label(self._tab_0,
+                  text="Welcome to \
+                   GeeksForGeeks").grid(column=0,
+                                        row=0,
+                                        padx=30,
+                                        pady=30)
 
-_update_plot.freq = FREQ_START
+    def _setup_plot(self):
+        fig = Figure(figsize=(5, 4), dpi=100)
 
-canvas = FigureCanvasTkAgg(fig, master=tab2)  # A tk.DrawingArea.
-canvas.draw()
-canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-canvas.get_tk_widget().after(10, _update_plot)
+        self._sine_freq = 1
+        t = np.arange(0, 3, .01)
 
-toolbar = NavigationToolbar2Tk(canvas, tab2)
-toolbar.update()
+        self._plot, = fig.add_subplot(111).plot(t, np.sin(self._sine_freq * np.pi * t))
 
-# placing the toolbar on the Tkinter window
-canvas.get_tk_widget().pack()
+        self._plot_canvas = FigureCanvasTkAgg(fig, master=self._tab_1)  # A tk.DrawingArea.
+        self._plot_canvas.draw()
 
-ttk.Frame()
+        self._plot_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self._plot_canvas.get_tk_widget().after(10, self._update_plot)
+
+        toolbar = NavigationToolbar2Tk(self._plot_canvas, self._tab_1)
+        toolbar.update()
+
+        self._plot_canvas.get_tk_widget().pack()
+
+    def _update_plot(self):
+        t = self._plot.get_xdata()
+
+        self._sine_freq = np.mod(self._sine_freq + 0.01, 10)
+        self._plot.set_ydata(np.sin(self._sine_freq * np.pi * t))
+
+        self._plot_canvas.draw()
+        self._plot_canvas.get_tk_widget().after(self.PLOT_RATE_MS, self._update_plot)
 
 
 def main():
     """
     Launch and run the non-linear 6dof simulator gui.
     """
-    root.mainloop()
-    # freq = FREQ_START
-    # while True:
-    #     root.update_idletasks()
-    #     root.update()
-    #     _update_plot(freq)
-    #     freq = np.mod(freq + 0.01, 10)
-    #     time.sleep(0.01)
+    gui = Gui()
+    gui.mainloop()
 
 
 if __name__ == "__main__":
