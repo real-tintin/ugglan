9cd5d3ebc32e8be2ddd71aef7899169c82cabaa4
Oscar Goldring <oscar@goldring.dev>
2023-08-11 16:15:43 +0200

    Add help to setup script and slim option.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,35 @@
-#!/bin/sh
-# Setup enviroment for project. Source
-# and run from the repo root.
-#
+#!/usr/bin/env bash
 
-# Setup python using venv.
-PY_VENV_ROOT="./venv"
+SLIM_SETUP=false
 
-python -m venv "${PY_VENV_ROOT}"
+VENV_ROOT="./venv"
+
+usage() {
+  echo "\
+Usage: source ./setup [OPTIONS]
+
+Use to setup local development environment.
+
+Options:
+    --slim      skip some steps in order to speed up the setup, useful if recently setup
+  " 1>&2
+}
+
+if [[ $# -gt 0 ]]; then
+    case $1 in
+        --slim)
+            SLIM_SETUP=true
+            ;;
+        --help)
+            usage
+            return
+            ;;
+        *)
+            usage
+            return
+            ;;
+    esac
+fi
 
 is_windows() {
   case "$(uname -sr)" in
@@ -19,18 +42,41 @@ is_windows() {
   esac
 }
 
-if is_windows; then
-    source "${PY_VENV_ROOT}/Scripts/activate"
+activate_venv() {
+  if is_windows; then
+      source "${VENV_ROOT}/Scripts/activate"
+  else
+      source "${VENV_ROOT}/bin/activate"
+  fi
+}
+
+setup_submodules() {
+    git submodule update --init --recursive
+}
+
+setup_python() {
+    python3 -m venv "${VENV_ROOT}"
+    activate_venv
+
+    python3 -m pip install --upgrade pip
+    python3 -m pip install -e ./tools
+}
+
+setup_python_slim() {
+  activate_venv
+}
+
+setup_env_vars() {
+  export DOCKER_BUILDKIT=1
+}
+
+if [[ "${SLIM_SETUP}" == "true" ]]; then
+    echo "Skipping some steps in order to speed up the setup"
+
+    setup_python_slim
+    setup_env_vars
 else
-    source "${PY_VENV_ROOT}/bin/activate"
+    setup_submodules
+    setup_python
+    setup_env_vars
 fi
-
-pip install --upgrade pip
-pip install -e ./tools
-
-# Init and update submodules.
-git submodule init
-git submodule update
-
-# Use docker buildx/buildkit by default.
-export DOCKER_BUILDKIT=1
